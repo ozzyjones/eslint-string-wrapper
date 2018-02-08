f6163c276a8ecc73f7c3a9b959fa1b31d37ef6b7
Brian Jones <brian.jones@geotoolkit.net>
2018-02-08 15:30:47 -0600

    Change Command Name => "ESLint: Wrap String"

--- src/extension.ts
+++ src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: ExtensionContext) {
     // The command has been defined in the package.json file
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
-    let disposable = commands.registerCommand('extension.sayHello', () => {
+    let disposable = commands.registerCommand('extension.wrapString', () => {
         // The code you place here will be executed every time your command is executed
 
         stringWrapper.wrapString();
